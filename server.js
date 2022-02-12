a803f2fb62210febce7993525e19cbf5a6ebba89
nickLee <ilkinm2003@gmail.com>
2022-02-12 18:04:07 +0400

    Add post method to /api/users/:_id/exercises route

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ const userSchema = new mongoose.Schema({
    username: String,
 });
 
+const exerciseSchema = new mongoose.Schema({
+   description: String,
+   duration: Number,
+   date: Date,
+});
+
 const User = new mongoose.model("User", userSchema);
+const Exercise = new mongoose.model("Exercise", exerciseSchema);
 
 app.get("/", (req, res) => {
    res.sendFile(__dirname + "/views/index.html");
@@ -37,6 +44,26 @@ app.post("/api/users", (req, res) => {
    newUser.save((err) => {
       if (err) return console.log(err);
       else res.json(newUser);
+   });
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+   const requestedId = req.params._id;
+   if (!req.body.date.length) {
+      req.body.date = new Date().toDateString();
+   }
+
+   User.findById(requestedId, (err, foundUser) => {
+      if (err || !foundUser) return console.log(err);
+      else {
+         res.json({
+            username: foundUser.username,
+            description: req.body.description,
+            duration: req.body.duration,
+            date: req.body.date,
+            _id: foundUser._id,
+         });
+      }
    });
 });
 

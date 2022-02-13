5a1dd3831b26d5a6debc7f900328294fad801201
nickLee <ilkinm2003@gmail.com>
2022-02-13 11:29:56 +0400

    Fix /api/users/:_id/exercise because previous one is not fixed

--- server.js
+++ server.js
@@ -14,14 +14,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const userSchema = new mongoose.Schema({
-   username: String
+   username: String,
 });
 
 const exerciseSchema = new mongoose.Schema({
-   _id: String,
    description: String,
    duration: Number,
-   date: Date,
+   date: String,
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -33,7 +32,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/users", (req, res) => {
    User.find({}, (err, foundUsers) => {
-      if (err || !foundUsers) return console.log(err);
+      if (err || !foundUsers) res.send(err);
       else res.json(foundUsers);
    });
 });
@@ -44,37 +43,39 @@ app.post("/api/users", (req, res) => {
    });
 
    newUser.save((err) => {
-      if (err) return console.log(err);
+      if (err) res.send(err);
       else res.json(newUser);
    });
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
-   const { _id, description, duration, date } = req.body;
+   const requestedId = req.params._id;
 
-   if (req.body.date === "") req.body.date = new Date().toDateString();
+   if (!req.body.date) {
+    req.body.date = new Date().toDateString();
+  }
+  
 
-   User.findById(_id, (err, data) => {
-      if (err || !data) res.send(err);
+   User.findById(requestedId, (err, foundUser) => {
+      if (err || !foundUser) res.send(err);
       else {
          const newExercise = new Exercise({
-            _id,
-            description,
-            duration,
-            date,
+            _id: req.body._id,
+            description: req.body.description,
+            duration: req.body.duration,
+            date: req.body.date,
          });
 
-         newExercise.save((err, data) => {
+         newExercise.save((err) => {
             if (err) return console.log(err);
-            else {
-               res.json({
-                  _id,
-                  username: data.username,
-                  description,
-                  duration,
-                  date,
-               });
-            }
+         });
+
+         res.json({
+            _id: foundUser._id,
+            username: foundUser.username,
+            date: newExercise.date,
+            duration: newExercise.duration,
+            description: newExercise.description,
          });
       }
    });
